6864c9485a071f856b04783be8f504e708b1d361
Danilo <dabio@users.noreply.github.com>
2019-03-24 22:39:01 +0100

    More packages

--- roles/packages/files/config.fish
+++ roles/packages/files/config.fish
@@ -6,8 +6,13 @@ if status --is-login
         set -x PATH $PATH /usr/local/go/bin
     end
 
+    if test -d $HOME/.poetry/bin
+        set -x PATH $PATH $HOME/.poetry/bin
+    end
+
     direnv hook fish | source
 end
 
 alias vi nvim
 alias vim nvim
+alias yum "yum --noplugins"
